7f758f652830a88e20c91df289b1a1a02093a926
Nima Karami <94768955+nima-karami@users.noreply.github.com>
2022-10-07 13:56:28 -0400

    updating playground

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
-import { useEffect, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import DigitalClock from './components/digital-clock/digital-clock.component';
 import TicTacToe from './components/tic-tac-toe/tic-tac-toe.component';
 
 function App() {
 
   return (
-    <TicTacToe />
+    <div className='container'>
+      <div className='card'>
+        <TicTacToe />
+      </div>
+      <div className='card'>
+        <DigitalClock />
+      </div>
+    </div>
   );
 }
 
